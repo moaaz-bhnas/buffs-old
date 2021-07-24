4421d73c09a3f6b88b80bd321830aee00c26cdb6
moaaz-bhnas <moaaz_bs@yahoo.com>
2021-07-24 18:55:32 +0200

    conditionally show dropdown indicator of react-select

--- src/components/review/Select.js
+++ src/components/review/Select.js
@@ -5,6 +5,14 @@ import { search } from "../../api";
 import PropTypes from "prop-types";
 import dateToYear from "../../utils/helpers/dateToYear";
 import movieNameWithReleaseDate from "../../utils/helpers/movieNameWithReleaseDate";
+import { sizes } from "../../utils/style";
+
+const customStyles = {
+  control: (provided) => ({
+    ...provided,
+    borderRadius: sizes.borderRadius.default,
+  }),
+};
 
 const Select = forwardRef(({ value, onChange, onFocus }, ref) => {
   const [session] = useSession();
@@ -12,9 +20,18 @@ const Select = forwardRef(({ value, onChange, onFocus }, ref) => {
   const firstName = fullName.split(" ")[0];
 
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const [dropdownIndicatorVisible, setDropdownIndicatorVisible] =
+    useState(false);
+
+  useEffect(() => {
+    setDropdownIndicatorVisible(results.length && query);
+  }, [query, results]);
 
   const loadOptions = useCallback(async (query) => {
     const { results } = await search(query);
+    setResults(results);
 
     const options = results.map((result) => {
       const label = movieNameWithReleaseDate(
@@ -45,6 +62,13 @@ const Select = forwardRef(({ value, onChange, onFocus }, ref) => {
       onFocus={onFocus}
       ref={ref}
       noOptionsMessage={() => null}
+      styles={customStyles}
+      components={
+        !dropdownIndicatorVisible && {
+          DropdownIndicator: () => null,
+          IndicatorSeparator: () => null,
+        }
+      }
       isClearable
       escapeClearsValue
     />
