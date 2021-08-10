de538526f71c0c04c0d172ca51bc71d4a6fd1730
moaaz-bhnas <moaaz_bs@yahoo.com>
2021-08-10 07:12:05 +0200

    add review function

--- src/db/index.js
+++ src/db/index.js
@@ -14,3 +14,15 @@ export const addMovie = async (movie) => {
 };
 
 // addReview
+export const addReview = async (review) => {
+  const { db } = await connectToDatabase();
+
+  const reviews = db.collection("reviews");
+  const result = await reviews.insertOne(review);
+
+  console.log(
+    `${result.insertedCount} documents were inserted to reviews collection with the _id: ${result.insertedId}`
+  );
+
+  return result;
+};
