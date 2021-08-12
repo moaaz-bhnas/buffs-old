842a36cbf0367d6cfa40c21d8d49c0c68391aef5
moaaz-bhnas <moaaz_bs@yahoo.com>
2021-08-12 07:50:41 +0200

    finished review form's mobile version

--- src/components/review/Loader.js
+++ src/components/review/Loader.js
@@ -1,29 +1,17 @@
 import { memo } from "react";
 import styled from "styled-components";
-import { css } from "@emotion/react";
 import BeatLoader from "react-spinners/BeatLoader";
 import { theme } from "../../utils/style";
 import PropTypes from "prop-types";
 
-// const override = css`
-//   display: block;
-//   margin: 0 auto;
-//   border-color: red;
-// `;
-
 const StyledLoader = styled.div`
-  margin-top: 1rem;
+  margin-top: ${({ loading }) => loading && "1rem"};
 `;
 
 const Loader = ({ loading }) => {
   return (
-    <StyledLoader>
-      <BeatLoader
-        color={theme.bg.grey4}
-        loading={loading}
-        // css={override}
-        size={10}
-      />
+    <StyledLoader loading={loading}>
+      <BeatLoader color={theme.bg.grey4} loading={loading} size={10} />
     </StyledLoader>
   );
 };
