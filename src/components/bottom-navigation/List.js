e8d76999efda64a9a9849619a30a912b17df5628
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-05-22 21:09:51 +0200

    profile page accessibility

--- src/components/bottom-navigation/List.js
+++ src/components/bottom-navigation/List.js
@@ -9,6 +9,7 @@ import BoldLoupe from "../svgs/BoldLoupe";
 import Loupe from "../svgs/Loupe";
 import Avatar from "../../containers/account-avatar/AccountAvatar";
 import Item from "./Item";
+import { useSession } from "next-auth/client";
 
 const StyledList = styled.ul`
   ${rawList}
@@ -17,34 +18,36 @@ const StyledList = styled.ul`
   justify-content: space-evenly;
 `;
 
-const list = [
-  {
-    ActiveIcon: FilledHome,
-    InactiveIcon: EmptyHome,
-    name: "home",
-    path: "/",
-  },
-  // {
-  //   ActiveIcon: BoldLoupe,
-  //   InactiveIcon: Loupe,
-  //   name: "search",
-  //   path: "/search",
-  // },
-  // {
-  //   ActiveIcon: FilledHeart,
-  //   InactiveIcon: EmptyHeart,
-  //   name: "notifications",
-  //   path: "/notifications",
-  // },
-  {
-    ActiveIcon: Avatar,
-    InactiveIcon: Avatar,
-    name: "account",
-    path: "/account",
-  },
-];
-
 const List = () => {
+  const [session] = useSession();
+
+  const list = [
+    {
+      ActiveIcon: FilledHome,
+      InactiveIcon: EmptyHome,
+      name: "home",
+      path: "/",
+    },
+    // {
+    //   ActiveIcon: BoldLoupe,
+    //   InactiveIcon: Loupe,
+    //   name: "search",
+    //   path: "/search",
+    // },
+    // {
+    //   ActiveIcon: FilledHeart,
+    //   InactiveIcon: EmptyHeart,
+    //   name: "notifications",
+    //   path: "/notifications",
+    // },
+    {
+      ActiveIcon: Avatar,
+      InactiveIcon: Avatar,
+      name: "account",
+      path: `/${session ? session.user.username : ""}`,
+    },
+  ];
+
   return (
     <StyledList>
       {list.map((item) => (
