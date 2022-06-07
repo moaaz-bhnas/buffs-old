77553484e666a657330e4d56856677ee0540da54
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-06-07 14:29:18 +0200

    pusher

--- src/lib/pusher.js
+++ src/lib/pusher.js
@@ -1,17 +1,10 @@
 import Pusher from "pusher";
 
-const {
-  NEXT_PUBLIC_PUSHER_APP_ID,
-  NEXT_PUBLIC_PUSHER_KEY,
-  NEXT_PUBLIC_PUSHER_SECRET,
-  NEXT_PUBLIC_PUSHER_CLUSTER,
-} = process.env;
-
 const pusher = new Pusher({
-  appId: NEXT_PUBLIC_PUSHER_APP_ID,
-  key: NEXT_PUBLIC_PUSHER_KEY,
-  secret: NEXT_PUBLIC_PUSHER_SECRET,
-  cluster: NEXT_PUBLIC_PUSHER_CLUSTER,
+  appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID,
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY,
+  secret: process.env.NEXT_PUBLIC_PUSHER_SECRET,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
   useTLS: true,
 });
 
